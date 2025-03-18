1a39466ced926516eba9e53c7950bad21090c3de
AbhinandanKumar001 <kumarabhinandan7860@gmail.com>
2025-03-18 11:16:58 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:"https://twitter-clone-kw2p.vercel.app/",
+    origin:"https://twitter-clone-kw2p.vercel.app",
     credentials:true
 }
 app.use(cors(corsOptions));
