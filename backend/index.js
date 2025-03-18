587fe618c553672db7fe5a04049285ad534eb9d0
AbhinandanKumar001 <kumarabhinandan7860@gmail.com>
2025-03-18 10:55:47 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import databaseConnection from "./config/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+import tweetRoute from "./routes/tweetRoute.js";
+import cors from "cors";
+import path from "path";
+
+dotenv.config({
+    path:".env"
+})
+databaseConnection();
+const app = express(); 
+const __dirname = path.resolve();
+
+// middlewares
+app.use(express.urlencoded({
+    extended:true
+}));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+    origin:"http://localhost:5173",
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+// api
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/tweet", tweetRoute);
+ 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3001}`);
+});
